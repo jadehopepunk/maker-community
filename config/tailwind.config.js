bf929140166b333855e9612388620f86129f3021
Craig Ambrose <craig@craigambrose.com>
2022-01-20 12:47:36 +1100

    Home page colour pallete

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const { colors } = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*'
-  ],
+  content: ["./app/helpers/**/*.rb", "./app/javascript/**/*.js", "./app/views/**/*"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "blue-dark": "#2a3166",
+        "skyblue-light": "#78CEE1",
+        "skyblue-dark": "#3E585D",
+        "pink-dark": "#ee7879",
+        "pink-very-dark": "#6D3F46",
+        "pink-light": "#f4abaa",
+        "teal-dark": "#0F766E",
+        beige: "#E1A994",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/typography")],
+};
