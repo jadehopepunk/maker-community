7953e61b706db07a0765214b518d70f4688c2d54
Craig Ambrose <craig@craigambrose.com>
2022-01-26 13:17:32 +1100

    added people index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events, only: %i[index show]
+
+  namespace :admin do
+    resources :people, only: [:index]
+  end
+
   root 'pages#home'
 end
