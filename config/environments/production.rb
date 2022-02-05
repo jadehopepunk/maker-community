9a8e13bf62c40196e6c29e1a965aaa0e8491af85
Craig Ambrose <craig@craigambrose.com>
2022-02-05 18:33:31 +1100

    mail config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'maker-community.heroku.com',
+    domain: 'maker-community.herokuapp.com',
     authentication: :plain
   }
 
