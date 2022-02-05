9c7b38821edf6691fef745a63f6c3dfb6f81db6b
Craig Ambrose <craig@craigambrose.com>
2022-02-05 16:02:39 +1100

    added mailgun mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_USERNAME'],
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    open_timeout: 5,
-    read_timeout: 5
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'maker-community.heroku.com',
+    authentication: :plain
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
