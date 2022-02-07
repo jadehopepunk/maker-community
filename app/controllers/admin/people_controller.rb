53ce81235bd543319243b00a7fc78323c3712f47
Craig Ambrose <craig@craigambrose.com>
2022-02-07 12:35:41 +1100

    added search box

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -6,7 +6,7 @@ module Admin
       @q = User.ransack(params[:q])
       @people = @q.result.includes(:active_plans).page(params[:page]).per(20)
       @plans = Plan.all
-      @search_params = params.permit(:page, :plan)
+      @search_params = params.permit(:page, :plan, q: [:s])
     end
 
     def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   rolify
+
   # Include default devise modules. Others available are:
   # , :registerable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
