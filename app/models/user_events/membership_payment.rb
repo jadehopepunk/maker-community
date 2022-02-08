e0d4f83e7d4f788621e5d44bd7f5e1954eefcaa0
Craig Ambrose <craig@craigambrose.com>
2022-02-08 22:24:53 +1100

    made timeline prettier

---
+++ app/models/user_events/membership_payment.rb
@@ -0,0 +1,11 @@
+module UserEvents
+  class MembershipPayment < UserEvent
+    def order_item
+      subject
+    end
+
+    def plan
+      order_item.product
+    end
+  end
+end
--- app/services/order_service.rb
+++ app/services/order_service.rb
@@ -2,6 +2,24 @@ class OrderService
   class << self
     def create(order)
       order.save!
+
+      order.order_items.each do |order_item|
+        trigger_order_item_events(order, order_item)
+      end
+
+      order
+    end
+
+    private
+
+    def trigger_order_item_events(order, order_item)
+      if order_item.product.is_a?(Plan)
+        UserEvents::MembershipPayment.create!(
+          user: order.user,
+          subject: order_item,
+          occured_at: order.completed_at
+        )
+      end
     end
   end
 end
