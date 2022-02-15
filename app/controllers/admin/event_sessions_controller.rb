980c1cc83ad62aeee2644fc3d17d45ba6118ba16
Craig Ambrose <craig@craigambrose.com>
2022-02-15 14:00:57 +1100

    display event session show page with a bookings table

--- app/controllers/admin/event_sessions_controller.rb
+++ app/controllers/admin/event_sessions_controller.rb
@@ -2,6 +2,10 @@ module Admin
   class EventSessionsController < AdminController
     def show
       @event_session = EventSession.find params[:id]
+
+      @q = @event_session.bookings.includes(:user).ransack(params[:q])
+      @q.sorts = ['users_display_name asc'] if @q.sorts.empty?
+      @bookings = @q.result.page(params[:page]).per(20)
     end
   end
 end
