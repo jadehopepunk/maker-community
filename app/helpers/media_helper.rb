2a2cb9ae00b8dd840b7e55950975580d107c839b
Craig Ambrose <craig@craigambrose.com>
2022-02-19 18:02:20 +1100

    event list is a column

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -1,4 +1,12 @@
 module MediaHelper
+  def image_model_tag(image, options = {})
+    default_options = {
+      alt: image.alt_text,
+      class: 'image-model'
+    }
+    image_tag image.file, default_options.merge(options)
+  end
+
   def variant_image_tag(attachment, variant_name, options = {})
     variant = attachment.variant(variant_name)
 
