e0c31b45a35856726f1963957c826465f9f19212
Craig Ambrose <craig@craigambrose.com>
2022-02-21 09:58:43 +1100

    correctly fetch the week

--- app/models/event_session.rb
+++ app/models/event_session.rb
@@ -2,7 +2,7 @@ class EventSession < ApplicationRecord
   belongs_to :event
   has_many :bookings, dependent: :destroy, class_name: 'EventBooking'
 
-  scope :from_this_week, -> { where('start_at >= ?', Date.today.prev_occurring(:monday).to_time) }
+  scope :from_this_week, -> { where('start_at >= ?', Date.today.beginning_of_week.to_time) }
 
   def time_range
     [start_at, end_at]
