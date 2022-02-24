4e8013a1eb48c4b711acf8cf08e2a7b6b3b009ea
Craig Ambrose <craig@craigambrose.com>
2022-02-24 16:01:35 +1100

    stick to using UTC

--- app/sync/wp/booking_availability.rb
+++ app/sync/wp/booking_availability.rb
@@ -72,7 +72,7 @@ module Wp
     attr_reader :type, :bookable, :priority, :from_time_string, :to_time_string, :from_date_string, :to_date_string
 
     def combine_date_and_time(date, time)
-      DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec, time.zone)
+      DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec)
     end
   end
 end
