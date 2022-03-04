76f49bcf1e97619c74ca0c833f28f6033ffa1a3f
Craig Ambrose <craig@craigambrose.com>
2022-03-04 19:21:46 +1100

    unplugged nights are correct weeks only

--- app/lib/events/unplugged_night.rb
+++ app/lib/events/unplugged_night.rb
@@ -8,6 +8,8 @@ module Events
       ''
     end
 
+    START_DATE = Date.new(2022, 3, 1)
+
     SESSIONS = {
       2 => {
         start_time: Time.parse('6:30pm UTC'),
@@ -20,13 +22,27 @@ module Events
 
       date_range.map do |date|
         template = SESSIONS[date.wday]
-        if SESSIONS[date.wday]
+        if template && valid_date?(date)
           result << Events::VirtualEventSession.new(self, date, template[:start_time],
                                                     template[:end_time])
         end
       end
 
       result
+    end
+
+    private
+
+    def valid_date?(date)
+      date >= START_DATE && valid_week?(date)
+    end
+
+    def valid_week?(date)
+      weeks_since_start(date).even?
+    end
+
+    def weeks_since_start(date)
+      ((date - START_DATE) / 7).to_i
     end
   end
 end
