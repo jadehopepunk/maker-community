447595a1584ee79d1fd3e976e967e35adb22c8a8
Craig Ambrose <craig@craigambrose.com>
2022-03-22 10:36:44 +1100

    added schedule notifications

--- app/models/event_session.rb
+++ app/models/event_session.rb
@@ -5,6 +5,7 @@ class EventSession < ApplicationRecord
   validates :start_at, presence: true
 
   scope :from_this_week, -> { where('start_at >= ?', Date.today.beginning_of_week.to_time) }
+  scope :in_date_range, ->(date_range) { where(start_at: date_range) }
   scope :tagged_with, lambda { |tags|
     return self if tags.empty?
 
---
+++ app/services/scheduled_notifications.rb
@@ -0,0 +1,26 @@
+class ScheduledNotifications
+  def initialize(date: Date.today, slack_notifier: SlackNotifier.new(delayed: false))
+    @slack_notifier = slack_notifier
+    @date = date
+  end
+
+  def start_of_week
+    start_date = closest_week_start
+    end_date = start_date.end_of_week
+    date_range = start_date..end_date
+    sessions = EventSession.in_date_range(date_range)
+
+    slack_notifier.upcoming_this_week(sessions:)
+  end
+
+  private
+
+  attr_reader :slack_notifier, :date
+
+  def closest_week_start
+    beginning = date.beginning_of_week
+    return beginning if beginning >= date
+
+    beginning + 7
+  end
+end
