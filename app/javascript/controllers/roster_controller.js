189610bfb24896e52615dcfcd89895b3313d7383
Craig Ambrose <craig@craigambrose.com>
2022-03-23 14:55:17 +1100

    highlights column to edit

--- app/javascript/controllers/roster_controller.js
+++ app/javascript/controllers/roster_controller.js
@@ -4,6 +4,8 @@ export default class extends Controller {
   static targets = ["edits"];
 
   connect() {
+    this.editing = null;
+
     this.editsTarget.querySelectorAll("[data-action=edit]").forEach((element) => {
       element.addEventListener("click", (element) => this.onEdit(element));
     });
@@ -13,10 +15,18 @@ export default class extends Controller {
     const userId = event.srcElement.dataset.user;
     this.startEditing(userId);
     event.preventDefault();
+
     return false;
   }
 
   startEditing(userId) {
+    if (this.editing) return;
+
+    this.editing = userId;
+    this.element.classList.add("editing");
+    this.element.querySelectorAll(`[data-user="${userId}"]`).forEach((element) => {
+      element.classList.add("editing");
+    });
     console.log("start editing", userId);
   }
 
