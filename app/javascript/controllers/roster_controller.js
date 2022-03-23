bc9c24554f7211e62a66aaeaee4894244c3b67c8
Craig Ambrose <craig@craigambrose.com>
2022-03-23 16:08:09 +1100

    can cancel

--- app/javascript/controllers/roster_controller.js
+++ app/javascript/controllers/roster_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
+const AVAILABILITIES = ["unknown", "busy", "available"];
+
 export default class extends Controller {
   static targets = ["edits", "rosterBody"];
 
@@ -31,20 +33,39 @@ export default class extends Controller {
   }
 
   onEditClick(cell) {
-    console.log("edit click", cell);
+    cell.dataset["availability"] = this.nextAvailability(cell.dataset["availability"]);
+  }
+
+  cancelEdit(event) {
+    this.editing = null;
+    this.clearEditingClass();
   }
 
   startEditing(userId) {
     if (this.editing) return;
 
     this.editing = userId;
+    this.clearEditingClass();
+    this.setEditingClass(userId);
+  }
+
+  // private
+
+  nextAvailability(input) {
+    return AVAILABILITIES[(AVAILABILITIES.indexOf(input) + 1) % AVAILABILITIES.length];
+  }
+
+  clearEditingClass() {
+    this.element.classList.remove("editing");
+    this.element.querySelectorAll(".editing").forEach((element) => {
+      element.classList.add("editing");
+    });
+  }
+
+  setEditingClass(userId) {
     this.element.classList.add("editing");
     this.element.querySelectorAll(`[data-user="${userId}"]`).forEach((element) => {
       element.classList.add("editing");
     });
-    console.log(this.rosterBodyTarget);
-    console.log("start editing", userId);
   }
-
-  // private
 }
