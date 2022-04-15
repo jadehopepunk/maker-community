b7de28ffc3ec79cb3c26384e6cabd2f764ecd6f8
Craig Ambrose <craig@craigambrose.com>
2022-04-15 16:41:05 +1000

    fix for friday notification

--- app/lib/date_utilities.rb
+++ app/lib/date_utilities.rb
@@ -3,11 +3,11 @@ class DateUtilities
     def date_range_to_datetime_range(date_range)
       return date_range if date_range.first.is_a?(DateTime)
 
-      (date_range.first.to_datetime)..((date_range.last + 1).to_datetime)
+      (date_range.first.to_time)..((date_range.last + 1).to_time)
     end
 
     def date_to_datetime_range(date)
-      date.to_datetime..(date.to_datetime + 1.day)
+      date.to_time..(date.to_time + 1.day)
     end
   end
 end
