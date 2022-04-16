05eea03c1073001d77b2ce5b363a729476d0457c
Craig Ambrose <craig@craigambrose.com>
2022-04-16 16:07:11 +1000

    can sign in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource_or_scope)
     stored_location_for(:user) || root_path
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    request.referrer
+  end
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,24 @@
 class BookingsController < ApplicationController
   def new
     load_event_session
-    @order = Forms::BookingOrder.new
+    @order = new_order
     @order.event_session = @event_session
     store_location_for(:user, request.fullpath)
   end
 
   def create
     load_event_session
-    @order = Forms::BookingOrder.new(event_session: @event_session)
+    @order = new_order
     @order.attributes = order_params
     @order.valid?
     render template: 'bookings/new'
   end
 
   private
+
+  def new_order
+    Forms::BookingOrder.new(event_session: @event_session, user: current_user)
+  end
 
   def load_event_session
     @event_session = EventSession.find(params[:event_id])
