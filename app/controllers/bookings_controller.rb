cfa51a8a75d9931163171bc3fbb408009fdf7670
Craig Ambrose <craig@craigambrose.com>
2022-04-16 12:10:44 +1000

    started simple form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,26 @@
 class BookingsController < ApplicationController
+  class BookingOrder
+    include ActiveModel::Model
+
+    attr_accessor :email
+
+    validates :email, presence: true
+  end
+
   def new
+    load_event_session
+    @order = BookingOrder.new
+  end
+
+  def create
+    load_event_session
+    @order = BookingOrder.new(params[:order].permit(:email))
+    render :new and return unless @order.valid?
+  end
+
+  private
+
+  def load_event_session
     @event_session = EventSession.find(params[:event_id])
   end
 end
