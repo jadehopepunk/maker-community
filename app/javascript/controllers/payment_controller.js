7ba2d3aeba334d4aa95021217d43c8ed717145c4
Craig Ambrose <craig@craigambrose.com>
2022-04-18 16:15:41 +1000

    added button spinner effect

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -2,9 +2,31 @@ import { Controller } from "@hotwired/stimulus";
 import { initializeStripeForm } from "../lib/stripe_event_payment";
 
 export default class extends Controller {
+  static targets = ["submit"];
+
   connect() {
     console.log("connected");
     initializeStripeForm(this.clientSecret);
+    this.element.addEventListener("submit", this.handleSubmit.bind(this));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setLoading(true);
+    console.log("handling submit");
+  }
+
+  setLoading(isLoading) {
+    if (isLoading) {
+      // Disable the button and show a spinner
+      this.element.querySelector("#submit").disabled = true;
+      this.element.querySelector("#spinner").classList.remove("hidden");
+      this.element.querySelector("#button-text").classList.add("hidden");
+    } else {
+      this.element.querySelector("#submit").disabled = false;
+      this.element.querySelector("#spinner").classList.add("hidden");
+      this.element.querySelector("#button-text").classList.remove("hidden");
+    }
   }
 
   // PRIVATE
