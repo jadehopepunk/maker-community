fb33dbfc4cfebb2fc5680403358682f52d1c2b69
Craig Ambrose <craig@craigambrose.com>
2022-04-19 19:48:08 +1000

    admin can set plan description

--- app/controllers/admin/plans_controller.rb
+++ app/controllers/admin/plans_controller.rb
@@ -6,8 +6,27 @@ module Admin
     end
 
     def edit
+      load_plan
+    end
+
+    def update
+      load_plan
+      if @plan.update(plan_params)
+        redirect_to admin_plans_path, notice: 'Plan was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def load_plan
       @plan = Plan.find(params[:id])
       authorize [:admin, @plan]
+    end
+
+    def plan_params
+      params.require(:plan).permit(:description)
     end
   end
 end
