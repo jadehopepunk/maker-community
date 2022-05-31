f3ebac14e5d407930c8b36dbdf129e55824a76cc
Craig Ambrose <craig@craigambrose.com>
2022-05-31 15:30:59 +1000

    fix error on images page

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class ImagesController < AdminController
     def index
-      @images = Image.page(params[:page]).order(:created_at, 'DESC').per(40)
+      @images = Image.page(params[:page]).order(created_at: 'DESC').per(40)
     end
   end
 end
