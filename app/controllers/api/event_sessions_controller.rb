ca6a0f0e87818c794192e52e4267a4e59a710fbe
Craig Ambrose <craig@craigambrose.com>
2023-08-30 18:29:52 +1000

    api done

--- app/controllers/api/event_sessions_controller.rb
+++ app/controllers/api/event_sessions_controller.rb
@@ -3,7 +3,20 @@ module Api
     def next
       @event_session = EventSession.today.first || EventSession.future.date_order.first
       render json: @event_session.as_json(
-        include: [:event, :manager_bookings]
+        only: [:id, :start_at, :end_at, :max_persons],
+        include: {
+          event: {
+            only: [:id, :slug, :title]
+          },
+          manager_bookings: {
+            only: [:id, :role],
+            include: {
+              user: {
+                only: [:id, :display_name]
+              }
+            }
+          }
+        }
       )
     end
   end
