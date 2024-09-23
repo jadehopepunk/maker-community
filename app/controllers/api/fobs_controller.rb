5578f0131cfde835e6edbec011c2bee870dc6fe3
Craig Ambrose <craig@craigambrose.com>
2024-09-23 16:47:23 +1000

    displays user if fob has one

--- app/controllers/api/fobs_controller.rb
+++ app/controllers/api/fobs_controller.rb
@@ -6,8 +6,18 @@ module Api
       fob_session = FobService.new.touch(params[:id])
       render json: {
         fob_id: fob_session.fob_id,
-        user: nil,
+        user: user_json(fob_session.fob.user),
         status: fob_session.status
+      }
+    end
+
+    private
+
+    def user_json(user)
+      return nil if user.nil?
+      {
+        id: user.id,
+        name: user.display_name
       }
     end
   end
