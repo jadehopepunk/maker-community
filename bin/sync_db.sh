62860ff81a4e950cc0891f51b1f318dae83fc887
jade <jade@hopepunk.me>
2025-02-16 16:43:49 +1100

    upgrade ruby version

--- bin/sync_db.sh
+++ bin/sync_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Capture DB backup"
-heroku pg:backups capture DATABASE_URL
+heroku pg:backups:capture DATABASE_URL
 
 echo "Download DB backup"
-curl -o tmp/latest.dump `heroku pg:backups public-url #{heroku_app_flag} | cat`
+curl -o tmp/latest.dump `heroku pg:backups:url #{heroku_app_flag} | cat`
 
 echo "Restore DB backup"
 pg_restore --clean --verbose --no-acl --no-owner -U postgres  -h localhost -d maker-community-development tmp/latest.dump
