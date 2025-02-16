925442e25fa19b1fcc9cc26e83a5c5c9e15bf1d0
jade <jade@hopepunk.me>
2025-02-16 16:52:08 +1100

    handle timezone for date range on open_times

--- app/lib/date_utilities.rb
+++ app/lib/date_utilities.rb
@@ -3,12 +3,16 @@ class DateUtilities
     def date_range_to_datetime_range(date_range)
       return date_range if date_range.first.is_a?(DateTime)
 
-      (date_range.first.to_time)..((date_range.last + 1).to_time)
+      (date_to_time_in_zone(date_range.first).beginning_of_day)..(date_to_time_in_zone(date_range.last).end_of_day)
     end
 
     def date_to_datetime_range(date)
       date_in_zone = date.to_time.in_time_zone(Time.zone)
       date_in_zone.beginning_of_day..date_in_zone.end_of_day
+    end
+
+    def date_to_time_in_zone(date)
+      date.to_time.in_time_zone(Time.zone)
     end
 
     def next_n_hours(number)
